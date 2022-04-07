49a15986e7edf3c90db78762570980a69fd9c747
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-04-07 18:59:36 +0100

    feat(utils): add throttle function

--- utils.go
+++ utils.go
@@ -1,10 +1,27 @@
 package main
 
-import "strings"
+import (
+	"strings"
+	"time"
+)
 
 func truncateText(s string, max int) string {
 	if max > len(s) {
 		return s
 	}
 	return s[:strings.LastIndex(s[:max], " ")] + "..."
+}
+
+func throttle(f func(), d time.Duration) func() {
+	shouldWait := false
+	return func() {
+		if !shouldWait {
+			f()
+			shouldWait = true
+			go func() {
+				<-time.After(d)
+				shouldWait = false
+			}()
+		}
+	}
 }
