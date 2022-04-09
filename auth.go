2a79a3d81e05cb791eb0c0dd28b916f5153e11c7
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-04-09 13:22:18 +0100

    refactor(auth): split out authentication logic

---
+++ auth.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+
+	"github.com/cli/oauth"
+	"github.com/cli/oauth/api"
+)
+
+// getOAuthToken authenticate the user with Github and return an access token
+func getOAuthToken() (*api.AccessToken, error) {
+	flow := &oauth.Flow{
+		Host:         oauth.GitHubHost("https://github.com"),
+		ClientID:     os.Getenv("OAUTH_CLIENT_ID"),
+		ClientSecret: os.Getenv("OAUTH_CLIENT_SECRET"),
+		CallbackURI:  "http://127.0.0.1/callback",
+		Scopes:       []string{"repo", "read:org", "gist"},
+	}
+
+	accessToken, err := flow.DetectFlow()
+	if err != nil {
+		return nil, err
+	}
+	return accessToken, nil
+}
--- main.go
+++ main.go
@@ -4,10 +4,7 @@ import (
 	"akinsho/gogazer/database"
 	"context"
 	"log"
-	"os"
 
-	"github.com/cli/oauth"
-	"github.com/cli/oauth/api"
 	"github.com/gdamore/tcell/v2"
 	"github.com/google/go-github/v43/github"
 	_ "github.com/joho/godotenv/autoload"
@@ -34,23 +31,6 @@ func inputHandler(event *tcell.EventKey) *tcell.EventKey {
 		app.Stop()
 	}
 	return event
-}
-
-// getOAuthToken authenticate the user with Github and return an access token
-func getOAuthToken() (*api.AccessToken, error) {
-	flow := &oauth.Flow{
-		Host:         oauth.GitHubHost("https://github.com"),
-		ClientID:     os.Getenv("OAUTH_CLIENT_ID"),
-		ClientSecret: os.Getenv("OAUTH_CLIENT_SECRET"),
-		CallbackURI:  "http://127.0.0.1/callback",
-		Scopes:       []string{"repo", "read:org", "gist"},
-	}
-
-	accessToken, err := flow.DetectFlow()
-	if err != nil {
-		return nil, err
-	}
-	return accessToken, nil
 }
 
 func main() {
